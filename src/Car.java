39158c9af1eb4459c6b46c56e850ee18e8ccea58
Yaroslav <yaroslavkutnenko@gmail.com>
2023-06-01 00:01:37 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    String brand, model;
+    int year;
+    public void startEngine()
+    {
+
+        System.out.println("Двигатель автомобиля запущен ");
+    }
+    public void stopEngine(){
+        System.out.println("Двигатель автомобиля остановлен ");
+
+    }
+
+    }
+
+
+
